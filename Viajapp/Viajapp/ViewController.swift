ccb26a642630d41737b2bc1e6f97d2a515813f8a
Joao Viana <joaobonilha96@gmail.com>
2021-03-22 11:29:33 -0300

    Adicionando objetos e implementando a Table View.

--- Viajapp/Viajapp/ViewController.swift
+++ Viajapp/Viajapp/ViewController.swift
@@ -7,12 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tabelaViagens: UITableView!
+    
+    let listaViagens: Array<String> = ["Rio de Janeiro", "Ceará", "São Paulo"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tabelaViagens.dataSource = self
     }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaViagens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = listaViagens[indexPath.row]
+        return cell
+    }
+
 
 
 }
